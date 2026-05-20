efff0bb873095fbefef9786017097911492aa843
Jaden Tripp <jadenitripp@gmail.com>
2026-05-19 20:46:38 -0600

    Preserve user probe state across syscalls

--- user/probe.asm
+++ user/probe.asm
@@ -18,15 +18,17 @@ PROBE_FLAG_READ_IWAD equ 0x04
 PROBE_FLAG_LSEEK equ 0x08
 
 start:
-    xor esi, esi
+    mov dword [probe_flags], 0
+    mov dword [buffer_ptr], 0
+    mov dword [wad_fd], 0
 
     mov eax, SYS_SBRK
     mov ebx, 64
     int 0x80
     cmp eax, 0xffffffff
     je .report
-    mov edi, eax
-    or esi, PROBE_FLAG_SBRK
+    mov [buffer_ptr], eax
+    or dword [probe_flags], PROBE_FLAG_SBRK
 
     mov eax, SYS_OPEN
     mov ebx, wad_path
@@ -34,33 +36,34 @@ start:
     int 0x80
     cmp eax, 0xffffffff
     je .report
-    mov ebp, eax
-    or esi, PROBE_FLAG_OPEN
+    mov [wad_fd], eax
+    or dword [probe_flags], PROBE_FLAG_OPEN
 
     mov eax, SYS_READ
-    mov ebx, ebp
-    mov ecx, edi
+    mov ebx, [wad_fd]
+    mov ecx, [buffer_ptr]
     mov edx, 12
     int 0x80
     cmp eax, 12
     jne .report
+    mov edi, [buffer_ptr]
     cmp dword [edi], 0x44415749
     jne .report
-    or esi, PROBE_FLAG_READ_IWAD
+    or dword [probe_flags], PROBE_FLAG_READ_IWAD
 
     mov eax, SYS_LSEEK
-    mov ebx, ebp
+    mov ebx, [wad_fd]
     mov ecx, 4
     xor edx, edx
     int 0x80
     cmp eax, 4
     jne .report
-    or esi, PROBE_FLAG_LSEEK
+    or dword [probe_flags], PROBE_FLAG_LSEEK
 
 .report:
     mov eax, SYS_USER_PROBE
     mov ebx, USER_PROBE_MAGIC
-    mov ecx, esi
+    mov ecx, [probe_flags]
     int 0x80
 
     mov eax, SYS_EXPECT_FAULT
@@ -75,3 +78,7 @@ start:
     jmp .hang
 
 wad_path db "DOOM1.WAD", 0
+align 4
+probe_flags dd 0
+buffer_ptr dd 0
+wad_fd dd 0
