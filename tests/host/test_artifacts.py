1555b1b694b64d12371ed8261c9f379cbe1a74b4
Jaden Tripp <jadenitripp@gmail.com>
2026-05-20 13:51:15 -0600

    Queue Doom persistence saves through ticcmd

--- tests/host/test_artifacts.py
+++ tests/host/test_artifacts.py
@@ -1112,6 +1112,7 @@ class SourceContractTests(unittest.TestCase):
             "fat_cache_table:",
             "fat_cache_root_dir:",
             "fat_find_writable_files:",
+            "fat_refresh_known_writable_slot:",
             "fat_find_persistence_markers:",
             "fat_create_root_file:",
             "fat_parse_user_root83:",
@@ -1166,10 +1167,18 @@ class SourceContractTests(unittest.TestCase):
         self.assertIn("call fat_bind_found_to_writable_slot", open_path)
         self.assertIn("call fd_alloc", open_path)
         self.assertIn("jc .bad_syscall_emfile", open_path)
+        self.assertIn("call fat_refresh_known_writable_slot", open_path)
+        self.assertIn("call fat_cache_table", open_path)
+        self.assertIn(".open_writable_refresh_generic:", open_path)
+        self.assertIn(".open_writable_truncate:", open_path)
         self.assertIn(".open_writable_bind_reserved:", open_path)
         self.assertIn(".open_writable_reserved_eio:", open_path)
         self.assertLess(
             open_path.index("call fd_alloc", open_path.index(".open_writable_ready:")),
+            open_path.index("call fat_refresh_known_writable_slot", open_path.index(".open_writable_ready:")),
+        )
+        self.assertLess(
+            open_path.index("call fat_refresh_known_writable_slot", open_path.index(".open_writable_ready:")),
             open_path.index("call fat_truncate_writable_file", open_path.index(".open_writable_ready:")),
         )
         self.assertIn("mov byte [fd_kinds + eax], FD_KIND_WAD", open_path)
@@ -1200,7 +1209,7 @@ class SourceContractTests(unittest.TestCase):
         self.assertIn(".validate_loop:", free_chain)
         self.assertIn(".validated:", free_chain)
         self.assertIn(".free_loop:", free_chain)
-        self.assertIn("mov [fat_next_free_hint], ebx", free_chain)
+        self.assertIn("mov dword [fat_next_free_hint], 2", free_chain)
         self.assertLess(free_chain.index(".validate_loop:"), free_chain.index(".validated:"))
         self.assertLess(free_chain.index(".validated:"), free_chain.index(".free_loop:"))
         validate_pass = free_chain.split(".validate_loop:", 1)[1].split(".validated:", 1)[0]
@@ -1209,6 +1218,17 @@ class SourceContractTests(unittest.TestCase):
         self.assertNotIn("call fat_write_cluster_entry", validate_pass)
         fat_write_locator = kernel.split("fat_file_lba_for_write:", 1)[1].split("fat_update_writable_size:", 1)[0]
         self.assertIn(".linked_new_cluster:", fat_write_locator)
+        self.assertIn("fat_lba_fail_stage", fat_write_locator)
+        self.assertIn("fat_lba_current_cluster", fat_write_locator)
+        self.assertIn("fat_lba_next_cluster", fat_write_locator)
+        self.assertIn("fat_lba_new_cluster", fat_write_locator)
+        self.assertIn("fat_lba_result_lba", fat_write_locator)
+        cluster_growth = fat_write_locator.split(".cluster_loop:", 1)[1].split(".have_cluster:", 1)[0]
+        self.assertLess(cluster_growth.index("call fat_next_cluster"), cluster_growth.index("cmp eax, 0"))
+        self.assertLess(cluster_growth.index("cmp eax, 0"), cluster_growth.index("je .allocate_next_cluster"))
+        self.assertLess(cluster_growth.index("je .allocate_next_cluster"), cluster_growth.index("cmp eax, 0xfff8"))
+        self.assertLess(cluster_growth.index("cmp eax, 0xfff8"), cluster_growth.index("jb .next_exists"))
+        self.assertLess(cluster_growth.index(".allocate_next_cluster:"), cluster_growth.index("call fat_alloc_cluster"))
         storage_init = kernel.split("storage_init:", 1)[1].split("fat_find_file:", 1)[0]
         self.assertLess(storage_init.index("call fat_cache_table"), storage_init.index("call fat_cache_root_dir"))
         pmm_init = kernel.split("pmm_init:", 1)[1].split("pmm_reserve_pages:", 1)[0]
