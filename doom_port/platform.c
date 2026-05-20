c7a7eed053baeda60e85b29df8e42dd606362058
Jaden Tripp <jadenitripp@gmail.com>
2026-05-20 11:16:21 -0600

    Restore Doom marker save readiness

--- doom_port/platform.c
+++ doom_port/platform.c
@@ -29,7 +29,6 @@ extern char savedescription[32];
 void doom_original_G_BuildTiccmd(ticcmd_t* cmd);
 void doom_original_G_Ticker(void);
 void G_DoSaveGame(void);
-void G_SaveGame(int slot, char* description);
 void G_LoadGame(char* name);
 
 static byte doom_zone[8 * 1024 * 1024];
@@ -68,7 +67,6 @@ static int load_checkpoint_done;
 #define VIBE_MUSIC_STREAM_TICS \
     ((int)((VIBE_MUSIC_STREAM_BYTES * 35u) / VIBE_MUSIC_DEFAULT_SAMPLE_RATE) / 16)
 #define VIBE_DOOM_SAVE_SCRATCH_BYTES 0x2c000u
-#define VIBE_PERSISTENCE_MIN_LEVELTIME 64
 
 static void report_doom_init_status(unsigned long flags)
 {
@@ -266,13 +264,6 @@ static int default_config_checkpoint_ready(void)
         && players[consoleplayer].mo;
 }
 
-static int persistence_action_ready(void)
-{
-    return default_config_checkpoint_ready()
-        && gametic >= VIBE_PERSISTENCE_MIN_LEVELTIME
-        && leveltime >= VIBE_PERSISTENCE_MIN_LEVELTIME;
-}
-
 static void checkpoint_default_config_if_needed(void)
 {
     if (default_config_checkpoint_checked || !defaultfile)
@@ -290,7 +281,7 @@ static void checkpoint_save_slot_if_needed(void)
 {
     if (save_checkpoint_done || !save_checkpoint_requested_once())
         return;
-    if (!persistence_action_ready()
+    if (!default_config_checkpoint_ready()
         || menuactive
         || sendsave
         || savedescription[0]
@@ -298,7 +289,10 @@ static void checkpoint_save_slot_if_needed(void)
         return;
     }
 
-    G_SaveGame(save_checkpoint_slot, "VIBE SAVE");
+    savegameslot = save_checkpoint_slot;
+    strcpy(savedescription, "VIBE SAVE");
+    sendsave = false;
+    gameaction = ga_savegame;
     save_checkpoint_done = 1;
 }
 
@@ -308,7 +302,7 @@ static void checkpoint_load_slot_if_needed(void)
 
     if (load_checkpoint_done || !load_checkpoint_requested_once())
         return;
-    if (!persistence_action_ready()
+    if (!default_config_checkpoint_ready()
         || menuactive
         || sendsave
         || savedescription[0]
